04514c9c4f1a5428f761ea14dbca329f9a120333
Colton <colton.foremaster@gmail.com>
2020-01-31 02:37:08 -0700

    Working pipes

--- tsh.c
+++ tsh.c
@@ -196,6 +196,8 @@ void execute_cmds(int *commands, int *in_redir, int *out_redir, char **argv, int
   int fds[num_pipes][2];
   int pid;
   int first_child_pid;
+  int stdin_cpy = dup(0);
+  int stdout_cpy = dup(1);
 
   for(int i = 0; i < num_pipes; i++) {
     const int pipe_status = pipe(&fds[i][0]);
@@ -216,15 +218,14 @@ void execute_cmds(int *commands, int *in_redir, int *out_redir, char **argv, int
       printf("Executing %s\n", cmd_name);
 
       if ( i < num_cmds - 1) {
-        FILE *read_stream = fdopen(fds[i][0], "r");
-        dup2(fds[i][0], 1);
-
-      } else {
-        close(fds[i][1]);
+        dup2(fds[i][1], 1);
       }
       if (i > 0) {
-        FILE *write_stream = fdopen(fds[i - 1][1], "w");
-        dup2(fds[i - 1][1], 0);
+        dup2(fds[i - 1][0], 0);
+      }
+      for (int j = 0; j < num_pipes; j++) {
+        close(fds[j][0]);
+        close(fds[j][1]);
       }
       execve(argv[commands[i]], &argv[commands[i]], env);
       exit(0);
@@ -233,6 +234,18 @@ void execute_cmds(int *commands, int *in_redir, int *out_redir, char **argv, int
         first_child_pid = pid;
       }
       int child_status;
+      close(fds[i][1]);
+      if (i > 0) {
+        close(fds[i - 1][0]);
+      }
+      if (i == num_cmds - 1) {
+        dup2(stdin_cpy, 0);
+        dup2(stdout_cpy, 1);
+        for (int j = 0; j < num_pipes; j++) {
+          close(fds[j][0]);
+          close(fds[j][1]);
+        }
+      }
       // printf("parent\n");
       wait(&child_status);
       // printf("PASSED HERE\n");
